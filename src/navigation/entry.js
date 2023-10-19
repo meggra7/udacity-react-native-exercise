e7363666d1797ae499a5e3b33ea65c7670ef3c3a
Meghan Dunham <meghan.g.dunham@gmail.com>
2023-10-19 10:51:06 -0400

    Add new navigation entry point to allow accessing Lesson 3

---
+++ src/navigation/entry.js
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TabNavigation from "./tab";
+import Entry from "../screens/DataManagementWithRedux/Entry";
+import AgileManifesto from "../screens/DataManagementWithRedux/AgileManifesto";
+
+const Stack = createNativeStackNavigator();
+
+export default function EntryNavigation() {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={Entry} />
+      <Stack.Screen name="Lessons One and Two" component={TabNavigation} />
+      <Stack.Screen name="Lesson Three" component={AgileManifesto} />
+    </Stack.Navigator>
+  );
+}
---
+++ src/screens/DataManagementWithRedux/Entry/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  lessonHeader: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 16,
+    marginBottom: 8,
+  },
+  exerciseButton: {
+    borderRadius: 8,
+    padding: 12,
+    marginHorizontal: 16,
+    marginVertical: 4,
+    alignSelf: "stretch",
+    alignItems: "center",
+    backgroundColor: "navy",
+  },
+  exerciseButtonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+    textAlign: "center",
+  },
+});
