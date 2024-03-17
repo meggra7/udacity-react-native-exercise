b1c901e9f20b65c798b700027165e11ec98e9173
Meghan Dunham <meghan.g.dunham@gmail.com>
2024-03-17 12:25:00 -0400

    Exercise: Redux Toolkit: Initialize the Redux store

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import EntryNavigation from "./src/navigation/entry";
+import { Provider } from "react-redux";
+import initializeStore from "./src/store";
+
+const store = initializeStore();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <EntryNavigation />
-    </NavigationContainer>
+    // Wrapping the app in the provider provides access to the Redux store from anywhere in the app
+    <Provider store={store}>
+      <NavigationContainer>
+        <EntryNavigation />
+      </NavigationContainer>
+    </Provider>
   );
 }
