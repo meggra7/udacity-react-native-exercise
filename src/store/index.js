54729ee352e02c039eacc1bcd030b380387fa5eb
Meghan Dunham <meghan.g.dunham@gmail.com>
2024-03-21 17:39:02 -0400

    Exercise: Async and Sagas: Set up sagas in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducers";
+import rootSaga from "./sagas";
+import createSagaMiddleware from "redux-saga";
 
 const initializeStore = (preloadedState = {}) => {
+  const sagaMiddleware = createSagaMiddleware();
+
   const store = configureStore({
     reducer,
     preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(sagaMiddleware),
   });
+
+  sagaMiddleware.run(rootSaga);
 
   return store;
 };
